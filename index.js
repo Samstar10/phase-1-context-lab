7bb1c15cdcd120b28287b6d0cafdc441f709fb27
Samuel Muli <samuelmuli95@gmail.com>
2023-10-10 17:01:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -21,3 +21,74 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord(data) {
+    return {
+      firstName: data[0],
+      familyName: data[1],
+      title: data[2],
+      payPerHour: data[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    };
+  }
+  
+  function createEmployeeRecords(data) {
+    return data.map(createEmployeeRecord);
+  }
+  
+  function createTimeInEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(' ');
+    this.timeInEvents.push({
+      type: 'TimeIn',
+      date: date,
+      hour: parseInt(hour, 10)
+    });
+    return this;
+  }
+  
+  function createTimeOutEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(' ');
+    this.timeOutEvents.push({
+      type: 'TimeOut',
+      date: date,
+      hour: parseInt(hour, 10)
+    });
+    return this;
+  }
+  
+  function hoursWorkedOnDate(date) {
+    const timeIn = this.timeInEvents.find(event => event.date === date);
+    const timeOut = this.timeOutEvents.find(event => event.date === date);
+    return (timeOut.hour - timeIn.hour) / 100;
+  }
+  
+  function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+  }
+  
+//   function allWagesFor() {
+//     const dates = this.timeInEvents.map(event => event.date);
+//     return dates.reduce((total, date) => total + wagesEarnedOnDate.call(this, date), 0);
+//   }
+  
+  function calculatePayroll(employees) {
+    return employees.reduce((total, employee) => total + allWagesFor.call(employee), 0);
+  }
+  
+  function findEmployeeByFirstName(employees, firstName) {
+    return employees.find(employee => employee.firstName === firstName);
+  }
+  
+  // Exporting the helper functions
+//   module.exports = {
+//     createEmployeeRecord,
+//     createEmployeeRecords,
+//     createTimeInEvent,
+//     createTimeOutEvent,
+//     hoursWorkedOnDate,
+//     wagesEarnedOnDate,
+//     allWagesFor,
+//     calculatePayroll,
+//     findEmployeeByFirstName
+//   };
+  
